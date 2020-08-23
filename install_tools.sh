ec0c96abe9980387ce06aab7c05fcf4cd02ce75f
masashi.g <masashi.g@gmail.com>
2020-08-23 17:44:44 +0900

    modify for install

--- install_tools.sh
+++ install_tools.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 # install toosl on Ubuntu 20.04
 
-apt install mysql-client -y
-snap install go --classic -y
+apt update
+apt install -y mysql-client
+snap install go --classic
+apt install -y nodejs
+apt install -y npm
 
 
 # node.js must be installed
 cd front
 npm install
 cd ..
+
+# env
+cp setup_env.sh.tmpl setup_env.sh
+
 
