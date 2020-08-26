9217b9304a8af083a9ad76bf2328e923b6aeb33a
mganeko <m.ganeko@gmail.com>
2020-08-26 17:35:49 +0900

    add for ubuntu18

---
+++ install_tools_ubuntu18.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# install toosl on Ubuntu 18.04
+
+apt update
+apt install -y mysql-client
+snap install go --classic
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+apt install -y nodejs
+apt install -y npm
+
+
+# node.js must be installed
+cd front
+npm install
+cd ..
+
+# env
+cp setup_env.sh.tmpl setup_env.sh
+
+
